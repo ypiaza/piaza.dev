f9b5fbdd776ccadbc9742d4498b8a8e6ebef30db
Yuri Piaza <yuripiazagomessantos@gmail.com>
2026-08-30 08:17:11 -0300

    Atualizar o projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,7 +2,7 @@ import auralabImg from '../assets/auralab.png';
 import crivusImg from '../assets/crivus.png';
 import advImg from '../assets/adveass.png';
 import clinica from '../assets/clinicaprot.png';
-import eleva form '../assets/eleva.jpg';
+import eleva from '../assets/eleva.jpg';
 
 export const projects = [
     {
